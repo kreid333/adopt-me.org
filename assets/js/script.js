c28fb03eb2fa93d0c8d7fd833dccac290fa8e503
kreid333 <kai.j.reid@gmail.com>
2021-07-31 04:21:16 -0400

    added gsap animations

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// HAMBURGER MENU
 const hamburger = document.querySelector(".hamburger");
 const menu = document.querySelector(".menu");
 
@@ -10,3 +11,45 @@ window.addEventListener("resize", () => {
     console.log(menu.classList.remove("show"));
   }
 });
+
+// GSAP ANIMATIONS
+let timeline = gsap.timeline();
+
+timeline
+  .fromTo(
+    ".brand",
+    {
+      x: -300,
+      opacity: 0,
+    },
+    {
+      x: 0,
+      opacity: 1,
+      duration: 1.3,
+    }
+  )
+  .fromTo(
+    ".navigation",
+    {
+      x: 300,
+      opacity: 0,
+    },
+    {
+      x: 0,
+      opacity: 1,
+      delay: 0.5,
+      duration: 1.3,
+    },
+    ">-1.4"
+  )
+  .fromTo(
+    ".hero-text",
+    {
+      opacity: 0,
+    },
+    {
+      opacity: 1,
+      delay: 0.5,
+    },
+    ">-0.9"
+  );

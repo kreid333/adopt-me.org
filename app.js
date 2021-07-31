bced2e0878c2e40966540fff101c85c95b0dfa74
kreid333 <kai.j.reid@gmail.com>
2021-07-31 12:31:43 -0400

    changed express port to 3000

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require("path");
 
 // DEFINING PORT
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // CREATING INSTANCE OF EXPRESS
 const app = express();

a879b8a0277dc8d13d249635a7ae8fb721035df5
kreid333 <kai.j.reid@gmail.com>
2021-07-31 12:28:50 -0400

    created express server to render html from and organized folder structure

---
+++ app.js
@@ -0,0 +1,28 @@
+// IMPORTING PACKAGES
+const express = require("express");
+const path = require("path");
+
+// DEFINING PORT
+const PORT = process.env.PORT || 3001;
+
+// CREATING INSTANCE OF EXPRESS
+const app = express();
+
+// MIDDLEWARE
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+app.use(
+  "/bootstrap",
+  express.static(path.join(__dirname, "./node_modules/bootstrap/dist/css"))
+);
+
+// LISTENING ON LOCALHOST
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`);
+});
+
+// RENDERING INDEX.HTML FOR ALL PATHS
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/index.html"));
+});
